26b0742c00bd1eab2e0776eecb935f304a3d4eda
Sergio Valente <plusrt1@gmail.com>
2022-08-15 21:21:12 -0500

    feat: adds widget & rate widget resources

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,25 +1,32 @@
 class WidgetsController < ApplicationController
   def index
     @widgets = [
-      OpenStruct.new(id: 1, name: "Stembolt"),
+      OpenStruct.new(id: 1234, name: "Stembolt"),
       OpenStruct.new(id: 2, name: "Flux Capacitor")
     ]
   end
 
   def show
+    widget_name =
+      if params[:id].to_i == 1234
+        "Stembolt"
+      else
+        "Widget #{params[:id]}"
+      end
+
     manufacturer = OpenStruct.new(
       id: rand(100),
       name: "Sector 7G",
       address: OpenStruct.new(
-              id: rand(100),
-              country: "UK"
+        id: rand(100),
+        country: "UK"
       )
     )
 
     @widget = OpenStruct.new(id: params[:id],
       manufacturer_id: manufacturer.id,
       manufacturer: manufacturer,
-      name: "Widget #{params[:id]}"
+      name: widget_name
     )
 
     def @widget.widget_id
