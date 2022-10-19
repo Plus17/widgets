417339df9dcf9564e0f997449afce48cb52102cd
Sergio <me@sergiovp.dev>
2022-10-19 14:24:18 -0500

    feat: adds widget creation logic

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -6,36 +6,28 @@ class WidgetsController < ApplicationController
     ]
   end
 
-  def show
-    widget_name =
-      if params[:id].to_i == 1234
-        "Stembolt"
-      else
-        "Widget #{params[:id]}"
-      end
+  def new
+    @widget = Widget.new
+    @manufacturers = Manufacturer.all
+  end
 
-    manufacturer = OpenStruct.new(
-      id: rand(100),
-      name: "Sector 7G",
-      address: OpenStruct.new(
-        id: rand(100),
-        country: "UK"
-      )
+  def create
+    @widget = Widget.create(
+      name: params.require(:widget)[:name],
+      price_cents: params.require(:widget)[:price_cents],
+      manufacturer_id: params.require(:widget)[:manufacturer_id],
+      widget_status: WidgetStatus.first
     )
 
-    @widget = OpenStruct.new(id: params[:id],
-      manufacturer_id: manufacturer.id,
-      manufacturer: manufacturer,
-      name: widget_name
-    )
-
-    def @widget.widget_id
-      if self.id.to_s.length < 3
-        self.id.to_s
-      else
-        self.id.to_s[0..-3] + "." +
-        self.id.to_s[-2..-1]
-      end
+    if @widget.valid?
+      redirect_to widget_path(@widget)
+    else
+      @manufacturers = Manufacturer.all
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @widget = Widget.find(params[:id])
   end
 end
