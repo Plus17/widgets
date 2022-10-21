bfaade0385e681206f1d8771a67fa04ee8fce83e
Sergio <me@sergiovp.dev>
2022-10-21 13:49:39 -0500

    feat: adds mailers

---
+++ test/mailers/finance_mailer_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class FinanceMailerTest < ActionMailer::TestCase
+  test "high_priced_widget" do
+    widget = FactoryBot.build(:widget)
+    mail = FinanceMailer.high_priced_widget(widget)
+    assert_equal "High priced widget", mail.subject
+    assert_equal ["finance@example.com"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match widget.name, mail.body.encoded
+  end
+
+end
